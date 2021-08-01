d0268f55aab5de59db08d67c7c98a3da5c3bf098
maikel233 <maikel233@live.nl>
2021-08-01 19:55:15 +0200

    Delete BunnyHop.h

--- BunnyHop.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace BHop
-{
-	void CreateMove(CUserCmd* cmd);
-	void CreateMoveCircle(CUserCmd* cmd);
-	bool doCircularStrafe(CUserCmd * pCmd, Vector & OriginalView);
-	void RotateMovement(CUserCmd * pCmd, float rotation);
-}
