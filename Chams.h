ee948662aa11a5730ff4fa285ea614b66aa2c23c
maikel233 <maikel233@live.nl>
2021-08-01 19:55:22 +0200

    Delete Chams.h

--- Chams.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace Chams {
-
-	extern IMaterial* chamsMaterial;
-	extern IMaterial* ignoredChamsMaterial;
-
-	void DrawModelExecute(IMatRenderContext* matctx, const DrawModelState_t& state, const ModelRenderInfo_t& pInfo, matrix3x4_t* pCustomBoneToWorld);
-}
