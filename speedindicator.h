e5e8b1bf491d12a270ecf6c4ec57aac00a562723
maikel233 <maikel233@live.nl>
2021-08-01 20:05:10 +0200

    Delete speedindicator.h

--- speedindicator.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace SpeedIndicator {
-	//Hooks
-	void Paint();
-	void RenderView(void* thisptr, CViewSetup& setup, CViewSetup& hudViewSetup, unsigned int nClearFlags, int whatToDraw);
-}
