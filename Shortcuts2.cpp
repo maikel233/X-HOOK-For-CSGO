6e16217f6f8c22060443d147940e82609a2fe598
maikel233 <maikel233@live.nl>
2021-08-01 20:05:55 +0200

    Delete Shortcuts2.cpp

--- Shortcuts2.cpp
+++
@@ -1,8 +0,0 @@
-#include "Features.h"
-
-void Shortcuts::SetKeyCodeState(ButtonCode_t code, bool bPressed) {
-#ifdef EXPERIMENTAL_SETTINGS
-	if (pInputSystem->IsButtonDown(ButtonCode_t::KEY_LALT) && code == ButtonCode_t::KEY_F && bPressed)
-		Settings::ThirdPerson::enabled = !Settings::ThirdPerson::enabled;
-#endif
-}
